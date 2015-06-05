f72213f1b3de5a97230a9a770243fd7fcc85d08e
Jared McFarland <jaredcolin@gmail.com>
2015-06-05 10:05:08 +0200

    Initial Commit

---
+++ AlertViewIssue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertViewIssue
+//
+//  Created by Jared McFarland on 6/5/15.
+//  Copyright (c) 2015 Jared McFarland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
